5c1438cf3baf987f5bd8f68addcac879ac6228d2
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-09-23 19:48:17 +0200

    complete enums

--- koans/src/beginner/AboutEnums.java
+++ koans/src/beginner/AboutEnums.java
@@ -7,7 +7,6 @@ import static com.sandwich.util.Assert.assertEquals;
 
 public class AboutEnums {
 
-
     enum Colors {
         Red, Blue, Green, Yellow // what happens if you add a ; here?
         // What happens if you type Red() instead?
@@ -16,33 +15,36 @@ public class AboutEnums {
     @Koan
     public void basicEnums() {
         Colors blue = Colors.Blue;
-        assertEquals(blue == Colors.Blue, __);
-        assertEquals(blue == Colors.Red, __);
-        assertEquals(blue instanceof Colors, __);
+        assertEquals(blue == Colors.Blue, true);
+        assertEquals(blue == Colors.Red, false);
+        assertEquals(blue instanceof Colors, true);
     }
 
     @Koan
     public void basicEnumsAccess() {
         Colors[] colorArray = Colors.values();
-        assertEquals(colorArray[2], __);
+        assertEquals(colorArray[2], Colors.Green);
     }
 
     enum SkatSuits {
         Clubs(12), Spades(11), Hearts(10), Diamonds(9);
 
-        SkatSuits(int v) {
+        SkatSuits(int v) { // getter for values of the enums
             value = v;
         }
 
-        private int value;
+        private int value; // declares if private or public
     }
 
     @Koan
     public void enumsWithAttributes() {
         // value is private but we still can access it. Why?
+        // encapsulation? Can access private values within the same class but not
+        // outside of it
         // Try moving the enum outside the AboutEnum class... What do you expect?
         // What happens?
-        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, __);
+        // error: value has private access in SkatSuits
+        assertEquals(SkatSuits.Clubs.value > SkatSuits.Spades.value, true);
     }
 
     enum OpticalMedia {
@@ -61,7 +63,7 @@ public class AboutEnums {
 
     @Koan
     public void enumsWithMethods() {
-        assertEquals(OpticalMedia.CD.getCoolnessFactor(), __);
-        assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), __);
+        assertEquals(OpticalMedia.CD.getCoolnessFactor(), -3500);
+        assertEquals(OpticalMedia.BluRay.getCoolnessFactor(), 490000);
     }
 }
