6397084bbfa5bb93e2f553b5f02bd9c6207b8970
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-09-24 18:54:38 +0200

    complete lambdas

--- koans/src/java8/AboutLambdas.java
+++ koans/src/java8/AboutLambdas.java
@@ -22,16 +22,16 @@ public class AboutLambdas {
 
     static String str = "";
 
-    //lambda has access to "this"
-    Caps thisLambdaField = s -> this.toString();
-    //lambda has access to object methods
-    Caps toStringLambdaField = s -> toString();
+    // lambda has access to "this"
+    Caps thisLambdaField = s -> this.toString(); // toString overridden in the class
+    // lambda has access to object methods
+    Caps toStringLambdaField = s -> toString(); // normal toString? Nope looks like both refer to overriden
 
     @Koan
     public void verySimpleLambda() throws InterruptedException {
-        Runnable r8 = () -> str = "changed in lambda";
+        Runnable r8 = () -> str = "changed in lambda"; // concurrency
         r8.run();
-        assertEquals(str, __);
+        assertEquals(str, "changed in lambda");
     }
 
     @Koan
@@ -40,51 +40,51 @@ public class AboutLambdas {
             return n.toUpperCase();
         };
         String capitalized = caps.capitalize("James");
-        assertEquals(capitalized, __);
+        assertEquals(capitalized, "JAMES");
     }
 
     @Koan
     public void simpleSuccinctLambda() {
-        //parameter type can be omitted,
-        //code block braces {} and return statement can be omitted for single statement lambda
-        //parameter parenthesis can be omitted for single parameter lambda
+        // parameter type can be omitted,
+        // code block braces {} and return statement can be omitted for single statement
+        // lambda
+        // parameter parenthesis can be omitted for single parameter lambda
         Caps caps = s -> s.toUpperCase();
         String capitalized = caps.capitalize("Arthur");
-        assertEquals(capitalized, __);
+        assertEquals(capitalized, "ARTHUR");
     }
 
     @Koan
     public void lambdaField() {
-        assertEquals(thisLambdaField.capitalize(""), __);
+        assertEquals(thisLambdaField.capitalize(""), "CAPS");
     }
 
     @Koan
     public void lambdaField2() {
-        assertEquals(toStringLambdaField.capitalize(""), __);
+        assertEquals(toStringLambdaField.capitalize(""), "CAPS");
     }
 
     @Koan
     public void effectivelyFinal() {
-        //final can be omitted like this:
+        // final can be omitted like this:
         /* final */
         String effectivelyFinal = "I'm effectively final";
         Caps caps = s -> effectivelyFinal.toUpperCase();
-        assertEquals(caps.capitalize(effectivelyFinal), __);
+        assertEquals(caps.capitalize(effectivelyFinal), "I'M EFFECTIVELY FINAL");
     }
 
     @Koan
     public void methodReference() {
         Caps caps = String::toUpperCase;
         String capitalized = caps.capitalize("Gosling");
-        assertEquals(capitalized, __);
+        assertEquals(capitalized, "GOSLING");
     }
 
     @Koan
     public void thisIsSurroundingClass() {
-        //"this" in lambda points to surrounding class
+        // "this" in lambda points to surrounding class
         Function<String, String> foo = s -> s + this.fieldFoo + s;
-        assertEquals(foo.apply("|"), __);
+        assertEquals(foo.apply("|"), "|Lambdas|");
     }
 
 }
-
