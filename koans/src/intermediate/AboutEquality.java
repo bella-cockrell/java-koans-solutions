a46e49d16bd17366858b481d31d6585d8fd8052f
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2023-09-24 13:13:43 +0200

    complete equality

--- koans/src/intermediate/AboutEquality.java
+++ koans/src/intermediate/AboutEquality.java
@@ -6,26 +6,27 @@ import static com.sandwich.koan.constant.KoanConstants.__;
 import static com.sandwich.util.Assert.assertEquals;
 
 public class AboutEquality {
-    // This suite of Koans expands on the concepts introduced in beginner.AboutEquality
+    // This suite of Koans expands on the concepts introduced in
+    // beginner.AboutEquality
 
     @Koan
     public void sameObject() {
         Object a = new Object();
         Object b = a;
-        assertEquals(a == b, __);
+        assertEquals(a == b, true);
     }
 
     @Koan
     public void equalObject() {
         Integer a = new Integer(1);
         Integer b = new Integer(1);
-        assertEquals(a.equals(b), __);
-        assertEquals(b.equals(a), __);
+        assertEquals(a.equals(b), true); // compares value
+        assertEquals(b.equals(a), true);
     }
 
     @Koan
     public void noObjectShouldBeEqualToNull() {
-        assertEquals(new Object().equals(null), __);
+        assertEquals(new Object().equals(null), false);
     }
 
     static class Car {
@@ -37,18 +38,27 @@ public class AboutEquality {
             horsepower = p;
         }
 
-        @Override
-        public boolean equals(Object other) {
+        // @Override //don't need to override if not extending?
+        public boolean equals(Car other) {
             // Change this implementation to match the equals contract
             // Car objects with same horsepower and name values should be considered equal
             // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#equals(java.lang.Object)
-            return false;
+            if (other != null && name.equals(other.name) && horsepower == other.horsepower) {
+                return true;
+            } else {
+                return false;
+            }
         }
 
         @Override
         public int hashCode() {
-            // @see http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
-            return super.hashCode();
+            // @see
+            // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
+            int result = 0;
+            if (name != null && horsepower > 0) {
+                result = name.hashCode();
+            }
+            return result;
         }
     }
 
@@ -89,12 +99,18 @@ public class AboutEquality {
         // As a general rule: When you override equals you should override
         // hash code
         // Read the hash code contract to figure out why
+        //
         // http://download.oracle.com/javase/6/docs/api/java/lang/Object.html#hashCode()
+        // (see below)
+        // If two objects are equal according to the equals(Object) method, then calling
+        // the hashCode method on each of the two objects must produce the same integer
+        // result.
 
         // Implement Car.hashCode around line 51 so that the following assertions pass
         Car car1 = new Car("Beetle", 50);
         Car car2 = new Car("Beetle", 50);
         assertEquals(car1.equals(car2), true);
+        // get the property's hash code instead, rather than the class's
         assertEquals(car1.hashCode() == car2.hashCode(), true);
     }
 
@@ -103,7 +119,11 @@ public class AboutEquality {
 
         @Override
         public int hashCode() {
-            return 4000;
+            int result = 0;
+            if (color != null) {
+                result = color.hashCode();
+            }
+            return result;
         }
 
         @Override
@@ -119,10 +139,17 @@ public class AboutEquality {
         Chicken chicken1 = new Chicken();
         chicken1.color = "black";
         Chicken chicken2 = new Chicken();
-        assertEquals(chicken1.equals(chicken2), __);
-        assertEquals(chicken1.hashCode() == chicken2.hashCode(), __);
+        Chicken chicken3 = new Chicken();
+        assertEquals(chicken1.equals(chicken2), false);
+        System.out.println(chicken1.hashCode());
+        assertEquals(chicken1.hashCode() == chicken2.hashCode(), false); // this should be false as 1 is black
+        assertEquals(chicken2.hashCode() == chicken3.hashCode(), true); // this should be true, as both are green
         // Does this still fit the hashCode contract? Why (not)?
+        // no because behaviour of .equals and .hashCode should be the same, but here
+        // they're not
+        // hashCode() will always return true for all instances of Chicken
         // Fix the Chicken class to correct this.
+
     }
 
 }
